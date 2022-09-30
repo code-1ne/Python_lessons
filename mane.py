6d6f96ab4fec6b0bb095df24849957c3e5549de9
Andrey <andreybaran23@gmail.com>
2022-09-30 23:16:52 +0300

    18 lesson

--- mane.py
+++ mane.py
@@ -1,3 +1,22 @@
-import cowsay as c
+class Cat:
+    name = None
+    age = None
+    isHappy = None
 
-print(c.get_output_string('trex', 'Hello (extinct) World'))
+    def set_data(self, name, age, isHappy):
+        self.name = name
+        self.age = age
+        self.isHappy = isHappy
+
+    def get_data(self):
+        print(self.name, "age:", self.age, ". Happy :", self.isHappy)
+
+
+cat1 = Cat()
+cat1.set_data("Барсик", 3, True)
+
+cat2 = Cat()
+cat2.set_data("Жопен", 2, False)
+
+cat1.get_data()
+cat2.get_data()
