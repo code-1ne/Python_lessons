8980a9b32d132d319795e0ca3427653bf4001eb6
Andrey <andreybaran@gmail.com>
2022-10-01 11:43:27 +0300

    18 lesson

--- mane.py
+++ mane.py
@@ -3,7 +3,11 @@ class Cat:
     age = None
     isHappy = None
 
-    def set_data(self, name, age, isHappy):
+    def __init__(self, name=None, age=None, isHappy=None):
+        self.set_data(name, age, isHappy)
+        self.get_data()
+
+    def set_data(self, name = None, age = None, isHappy = None):
         self.name = name
         self.age = age
         self.isHappy = isHappy
@@ -11,12 +15,7 @@ class Cat:
     def get_data(self):
         print(self.name, "age:", self.age, ". Happy :", self.isHappy)
 
-
 cat1 = Cat()
-cat1.set_data("Барсик", 3, True)
+cat1.set_data("John", 2)
 
-cat2 = Cat()
-cat2.set_data("Жопен", 2, False)
-
-cat1.get_data()
-cat2.get_data()
+cat2 = Cat("Жопен", 2, False)
